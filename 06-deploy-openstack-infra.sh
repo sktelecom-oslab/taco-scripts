2706f10950e0396242435b5fd8306af5a169fe2e
daeseong.kim <powerds0111@gmail.com>
2017-12-04 18:45:55 +0900

    seperate ceph and openstack-infra

--- 06-deploy-openstack-infra.sh
+++ 06-deploy-openstack-infra.sh
@@ -8,8 +8,7 @@ helm install -f ~/taco-values/single/ingress.yaml ~/openstack-helm/ingress/ --na
 helm install -f ~/taco-values/single/etcd.yaml ~/openstack-helm/etcd/ --namespace openstack --name etcd
 helm install -f ~/taco-values/single/rabbitmq.yaml ~/openstack-helm/rabbitmq/ --namespace openstack --name rabbitmq
 helm install -f ~/taco-values/single/memcached.yaml ~/openstack-helm/memcached/ --namespace openstack --name memcached
-helm install -f ~/taco-values/single/ceph.yaml ~/openstack-helm/ceph/ --namespace ceph --name ceph --set network.public=$CIDR --set network.cluster=$CIDR
 
 helm install -f ~/taco-values/single/ceph.yaml --namespace=openstack --name=ceph-openstack-config ~/openstack-helm/ceph/ --set network.public=$CIDR --set network.cluster=$CIDR --set deployment.storage_secrets=false  --set deployment.ceph=false  --set deployment.rbd_provisioner=false  --set deployment.client_secrets=true  --set deployment.rgw_keystone_user_and_endpoints=false --set bootstrap.enabled=false
-sleep 5
+sleep 3
 helm install -f ~/taco-values/single/mariadb.yaml ~/openstack-helm/mariadb/ --namespace openstack --name mariadb
