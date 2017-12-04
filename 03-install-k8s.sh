7be6e73fb69742ca63ccfba5013984292e64451b
daeseong.kim <powerds0111@gmail.com>
2017-12-04 17:18:50 +0900

    check dir exists

--- 03-install-k8s.sh
+++ 03-install-k8s.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
 cd ~/
+KUBESPRAY_DIR="~/taco-kubespray"
+if [ -d "$KUBESPRAY_DIR" ]; then
+  rm -rf $KUBESPRAY_DIR
+fi
 git clone https://github.com/sktelecom-oslab/taco-kubespray.git
 ansible-playbook -u root -b -i ~/taco-kubespray/inventory/taco-aio.cfg ~/taco-kubespray/cluster.yml
 
--- 04-init-helm.sh
+++ 04-init-helm.sh
@@ -5,8 +5,16 @@ helm serve . &
 sleep 3
 helm repo add localhost http://localhost:8879/charts
 cd ~/
+HELM_DIR="~/openstack-helm"
+if [ -d "$HELM_DIR" ]; then
+  rm -rf $HELM_DIR
+fi
 git clone https://github.com/openstack/openstack-helm.git
 cd openstack-helm
 make
 cd ~/
+VALUES_DIR="~/taco-values"
+if [ -d "$VALUES_DIR" ]; then
+  rm -rf $VALUES_DIR
+fi
 git clone https://github.com/sktelecom-oslab/taco-values.git
