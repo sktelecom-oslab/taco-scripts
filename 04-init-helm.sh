d081b8e97f1384747d9ea4fcac6893f926d1e903
Hyunsun Moon <hyunsun.moon@gmail.com>
2017-12-04 20:45:10 +0900

    fixed openstack helm master version

--- 04-init-helm.sh
+++ 04-init-helm.sh
@@ -10,6 +10,7 @@ if [ -d $HELM_DIR ]; then
   rm -rf $HELM_DIR
 fi
 git clone https://github.com/openstack/openstack-helm.git
+git checkout cae76dc9ab94f48259a8514075efb76f088a2722
 cd openstack-helm
 make
 cd ~/
