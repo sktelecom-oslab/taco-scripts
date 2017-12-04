f2d7e73916e9f43f0c0a8ca97e421f68656058d6
Siri Kim <siri.kim1012@gmail.com>
2017-12-04 17:48:05 +0900

    move locale setting to the first

--- 01-init-env.sh
+++ 01-init-env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ex
+
+export LC_ALL="en_US.UTF-8"
 apt-get update
 apt-get -y upgrade
 apt install -y python python-pip git jq nmap bridge-utils ipcalc
@@ -11,4 +13,3 @@ apt install -y sudo ceph-common
 
 swapoff -a
 modprobe rbd
-export LC_ALL="en_US.UTF-8"
