ce256758e96cc7b179ce55be865a67217feef883
daeseong.kim <powerds0111@gmail.com>
2017-12-04 19:42:43 +0900

    add python-openstackclient

--- 01-init-env.sh
+++ 01-init-env.sh
@@ -5,11 +5,12 @@ export LC_ALL="en_US.UTF-8"
 apt-get update
 apt-get -y upgrade
 apt install -y python python-pip git jq nmap bridge-utils ipcalc
+apt install -y ceph-common
 pip install --upgrade pip
 pip install -U ansible
 pip install netaddr
 pip install pyOpenSSL==16.2.0
-apt install -y sudo ceph-common
+pip install python-openstackclient==3.8.1
 
 swapoff -a
 modprobe rbd
