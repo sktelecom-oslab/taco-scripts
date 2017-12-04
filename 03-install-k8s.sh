de178a0a348d1887698124eb0f79df57a8b126a0
daeseong.kim <powerds0111@gmail.com>
2017-12-04 13:45:10 +0900

    change to init helm

--- 03-install-k8s.sh
+++ 03-install-k8s.sh
@@ -3,3 +3,12 @@ set -ex
 cd ~/
 git clone https://github.com/sktelecom-oslab/taco-kubespray.git
 ansible-playbook -u root -b -i ~/taco-kubespray/inventory/taco-aio.cfg ~/taco-kubespray/cluster.yml
+
+kubectl label nodes openstack-control-plane=enabled --all --namespace=openstack --overwrite
+kubectl label nodes openvswitch=enabled --all --namespace=openstack --overwrite
+kubectl label nodes openstack-compute-node=enabled --all --namespace=openstack --overwrite
+kubectl label nodes kubernetes-control-plane=enabled --all --overwrite
+
+kubectl create clusterrolebinding openstack \
+--clusterrole=cluster-admin \
+--serviceaccount=openstack:default
---
+++ 04-init-helm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+
+helm serve . &
+sleep 3
+helm repo add localhost http://localhost:8879/charts
+cd ~/
+git clone https://github.com/openstack/openstack-helm.git
+cd openstack-helm
+make
