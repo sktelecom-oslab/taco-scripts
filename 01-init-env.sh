7a52e94a397018d5d4fd679e4b278b5b92c0d570
root <hyunsun.moon@gmail.com>
2017-12-05 10:57:09 +0900

    Added version requirements for Kubespray dependencies
    Also make ceph-common to be installed after installing pyOpenSSL
    with pip to avoid conflict with apt.

--- 01-init-env.sh
+++ 01-init-env.sh
@@ -2,15 +2,20 @@
 set -ex
 
 export LC_ALL="en_US.UTF-8"
+
 apt-get update
 apt-get -y upgrade
-apt install -y python python-pip git jq nmap bridge-utils ipcalc
-apt install -y ceph-common
+
+apt install -y python python-pip
 pip install --upgrade pip
-pip install -U ansible
+pip install pbr>=1.6
+pip install ansible>=2.4.0
 pip install netaddr
+pip install jinja2>=2.9.6
 pip install pyOpenSSL==16.2.0
 pip install python-openstackclient==3.12.0
+
+apt install -y ceph-common git jq nmap bridge-utils ipcalc
 
 swapoff -a
 modprobe rbd
