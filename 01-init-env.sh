b8cb9f0d6cad4cb03f7e04a4848871eb83380fe3
daeseong.kim <daeseong.kim@sk.com>
2017-12-08 15:03:12 +0900

    add ' between pip install package name

--- 01-init-env.sh
+++ 01-init-env.sh
@@ -12,12 +12,12 @@ apt-get -y upgrade
 
 apt install -y python python-pip
 pip install --upgrade pip
-pip install pbr>=1.6
-pip install ansible>=2.4.0
-pip install netaddr
-pip install jinja2>=2.9.6
-pip install pyOpenSSL==16.2.0
-pip install python-openstackclient==3.12.0
+pip install 'pbr>=1.6'
+pip install 'ansible>=2.4.0'
+pip install 'netaddr'
+pip install 'jinja2>=2.9.6'
+pip install 'pyOpenSSL==16.2.0'
+pip install 'python-openstackclient==3.12.0'
 
 apt install -y ceph-common git jq nmap bridge-utils ipcalc
 
