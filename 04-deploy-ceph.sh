a2b37dee5075224168e1bc830318e5c115e1c63f
Dae Seong Kim <powerds0111@gmail.com>
2017-12-19 17:11:59 +0900

    fix to deploy openstack by armada

--- 04-deploy-ceph.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-(( EUID )) && echo You need to be root. && exit 1
-set -ex
-EXIP=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $7}')
-EXGW=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $3}')
-CIDR=$(ipcalc -n $EXIP $EXGW | awk /'Network:'/'{print $2}')
-
-helm install -f ~/taco-values/single/ceph.yaml ~/openstack-helm/ceph/ --namespace ceph --name ceph --set network.public=$CIDR --set network.cluster=$CIDR
