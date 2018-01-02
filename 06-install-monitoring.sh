6f8407a1e1fe2cb4a76cecebf48f4b83930b9d2a
Siri Kim <siri.kim1012@gmail.com>
2018-01-02 00:40:52 -0800

    remove useless file

--- 06-install-monitoring.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-(( EUID )) && echo You need to be root. && exit 1
-set -ex
-cd ~/
-ansible-playbook -u root -b -i ~/taco-kubespray/inventory/taco-aio.cfg ~/taco-kubespray/monitoring.yml
