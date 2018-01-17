c05ef964bfebf40152d21f4b9b081161c4bdfd97
sungil <usnexp@gmail.com>
2018-01-17 15:20:59 +0900

    Solve the issue#7

--- 06-install-monitoring.sh
+++ 06-install-monitoring.sh
@@ -2,4 +2,4 @@
 (( EUID )) && echo You need to be root. && exit 1
 set -ex
 cd ~/
-ansible-playbook -u root -b -i ~/taco-kubespray/inventory/taco-aio.cfg ~/taco-kubespray/monitoring.yml
+ansible-playbook -u root -b -i ~/apps/taco-kubespray/inventory/taco-aio.cfg ~/apps/taco-kubespray/monitoring.yml
